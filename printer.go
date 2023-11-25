c1da05c9ce25757a709134b447a2396cafc94e1f
Larry Zhao <thehiddendepth@gmail.com>
2023-11-25 13:53:04 +0800

    fix print

--- printer.go
+++ printer.go
@@ -9,15 +9,15 @@ import (
 var PrintVerbosly bool = false
 
 func PrintInfo(format string, args ...interface{}) {
-	fmt.Printf("%s %s.\n", color.GreenString("•"), fmt.Sprintf(format, args...))
+	fmt.Printf("%s %s\n", color.GreenString("•"), fmt.Sprintf(format, args...))
 }
 
 func PrintVerbose(format string, args ...interface{}) {
 	if PrintVerbosly {
-		fmt.Printf("%s %s.\n", color.CyanString("•"), fmt.Sprintf(format, args...))
+		fmt.Printf("%s %s\n", color.CyanString("•"), fmt.Sprintf(format, args...))
 	}
 }
 
 func PrintError(format string, args ...interface{}) {
-	fmt.Printf("%s %s.\n", color.RedString("•"), fmt.Sprintf(format, args...))
+	fmt.Printf("%s %s\n", color.RedString("•"), fmt.Sprintf(format, args...))
 }
